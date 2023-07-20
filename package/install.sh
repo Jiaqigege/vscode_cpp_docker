36d280432a0b3452a5e4b9cf13fef1a4613a0dcd
Damon <lixsh1130@163.com>
2023-07-20 01:19:16 +0000

    .

--- package/install.sh
+++ package/install.sh
@@ -15,36 +15,11 @@ install_clang_extra_tools() {
 		return 1
 	fi
 
-	# 提取 clang_extra_tools 版本号
-	clang_extra_tools_version=$(echo clang_extra_tools_*.tar.gz | grep -oP '(?<=clang_extra_tools_)\d+\.\d+\.\d+')
-
-	if [ -z "$clang_extra_tools_version" ]; then
-		echo "无法提取 clang_extra_tools 版本号。"
-		return 1
-	else
-		echo "clang_extra_tools 版本号：$clang_extra_tools_version"
-	fi
-
-	# 提取主机架构
-	host_arch=$(echo clang_extra_tools_*.tar.gz | grep -oP '(?<=clang_extra_tools_)\d+\.\d+\.\d+_(\S+)_\d+\.\d+')
-
-	if [ -z "$host_arch" ]; then
-		echo "无法提取主机架构。"
-	else
-		echo "主机架构：$host_arch"
-	fi
-
-	# 提取 GCC 版本
-	gcc_version=$(echo clang_extra_tools_*.tar.gz | grep -oP '(?<=_)\d+\.\d+(?=\.tar\.gz)')
-	if [ -z "$gcc_version" ]; then
-		echo "无法提取 GCC 版本。"
-	else
-		echo "GCC 版本：$gcc_version"
-	fi
+	complete_dir = $(echo clang_extra_tools_*.tar.gz)
 
 	tar -xzvf clang_extra_tools*.tar.gz -C $(cwd)/tmp
-	chmod +x $(cwd)/tmp/*
-	mv $(cwd)/tmp/* /usr/bin/
+	chmod +x $(cwd)/tmp/${complete_dir}/*
+	mv $(cwd)/tmp/${complete_dir}/* /usr/bin/
 	rm $(cwd)/tmp -rf
 
 	# 完成安装
@@ -61,8 +36,8 @@ install_aarch64_compiler_7.5()
 		return 1
 	fi
 
-	xz -d gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
-	tar -xvf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar -C /opt
+	xz -d ${cwd}/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
+	tar -xvf ${cwd}/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar -C /opt
 	update-alternatives --install /usr/bin/aarch64-linux-gnu aarch64-linux-gnu /opt/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu 80
 	echo "PATH=$PATH:/usr/bin/aarch64-linux-gnu/bin" >> /home/damon/.bashrc
 	rm -rf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar
