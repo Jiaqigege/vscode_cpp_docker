0b138bc06ceed1844d6061deb1514e38caaa8b0e
Jiaqigege <lixsh1130@163.com>
2023-07-27 16:52:02 +0800

    feat: complete.

--- package/install.sh
+++ package/install.sh
@@ -7,20 +7,11 @@ cwd=$(
 
 # 定义函数：安装 clang_extra_tools
 install_clang_extra_tools() {
-	# 检查 clang_extra_tools 压缩包是否存在
-	if [ -e ${cwd}/clang_extra_tools_*.tar.gz ]; then
-		echo "找到 clang_extra_tools 压缩包。"
-	else
-		echo "未找到 clang_extra_tools 压缩包。无法继续安装。"
-		return 1
-	fi
-
-	complete_dir = $(echo clang_extra_tools_*.tar.gz)
-
-	tar -xzvf clang_extra_tools*.tar.gz -C $(cwd)/tmp
-	chmod +x $(cwd)/tmp/${complete_dir}/*
-	mv $(cwd)/tmp/${complete_dir}/* /usr/bin/
-	rm $(cwd)/tmp -rf
+	mkdir -p ${cwd}/tmp
+	tar -xzvf ${cwd}/clang_extra_tools_16.0.6_x86_64_9.4.tar.gz -C ${cwd}/tmp
+	chmod +x ${cwd}/tmp/clang_extra_tools_16.0.6_x86_64_9.4/*
+	mv ${cwd}/tmp/clang_extra_tools_16.0.6_x86_64_9.4/* /usr/bin/
+	rm ${cwd}/tmp -rf
 
 	# 完成安装
 	echo "软件安装完成。"
@@ -39,7 +30,7 @@ install_aarch64_compiler_7.5()
 	xz -d ${cwd}/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
 	tar -xvf ${cwd}/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar -C /opt
 	update-alternatives --install /usr/bin/aarch64-linux-gnu aarch64-linux-gnu /opt/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu 80
-	echo "PATH=$PATH:/usr/bin/aarch64-linux-gnu/bin" >> /home/damon/.bashrc
+	echo "export PATH=$PATH:/usr/bin/aarch64-linux-gnu/bin" >> /home/damon/.bashrc
 	rm -rf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar
 }
 
